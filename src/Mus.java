f6ae1b0b494049e629ad5f045094f3330c1db2d7
sune <sune1309@edu.easj.dk>
2019-12-03 14:52:03 +0100

    innital commmit

--- src/Mus.java
+++ src/Mus.java
@@ -6,6 +6,7 @@ public class Mus {
     public int min = 60;
     public int numofnuts;
     private int valgtNut;
+    public boolean nucount[];
 
     public int usrguess;
 
@@ -13,26 +14,29 @@ public class Mus {
     public void gennuts() {
         numofnuts = ThreadLocalRandom.current().nextInt(min, max + 1);
         System.out.println(numofnuts + "peppernødder generet");
+        nucount = new boolean[numofnuts];
+        Arrays.fill(nucount, true);
     }
+
+
     //choosenut
-    private int valgtnut(){
-        valgtNut = ThreadLocalRandom.current().nextInt(numofnuts-1+1);
+    private int valgtnut() {
+        valgtNut = ThreadLocalRandom.current().nextInt(numofnuts - 1 + 1);
         System.out.println("den valgte nød er" + valgtNut);
         return valgtNut;
     }
+
     public void Usrguess() {
         Scanner scanner = new Scanner(System.in);
         while (true) {
-        usrguess = scanner.nextInt();
-        if (usrguess== valgtNut) {
-            System.out.println("mus!");
-            valgtnut();
-            System.out.println("ny nød er valgt");
-        }
-
-        else {
-            System.out.println("Haps");
+            usrguess = scanner.nextInt();
+            if (usrguess == valgtNut) {
+                System.out.println("mus!");
+                valgtnut();
+                System.out.println("ny nød er valgt");
+            } else {
+                System.out.println("Haps");
+            }
         }
     }
-}
 }
